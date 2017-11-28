93afccda3f4b9bfd322557073921df3105f4a0f9
Lindemberg de Castro <lindembergncastro@outlook.com>
2017-11-28 19:04:57 -0300

    second commit

---
+++ components/Decks.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+class Decks extends Component{
+
+    render(){
+        return(
+            <View>
+            <Text>Decks</Text>
+            </View>
+        )
+    }
+
+}
+
+export default Decks
